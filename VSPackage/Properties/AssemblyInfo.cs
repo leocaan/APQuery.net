8b79ac402d5c799fc6e8642a468f61e8e1057444
leocaan <leocaan@qq.com>
2015-06-29 12:53:40 +0800

    mini version++

--- VSPackage/Properties/AssemblyInfo.cs
+++ VSPackage/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSPackage")]
+[assembly: AssemblyTitle("APQuery Add-in")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("APQuery.NET")]
-[assembly: AssemblyProduct("VSPackage")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Symber")]
+[assembly: AssemblyProduct("Symber.Web.APQuery")]
+[assembly: AssemblyCopyright("Copyright © Symber 2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
